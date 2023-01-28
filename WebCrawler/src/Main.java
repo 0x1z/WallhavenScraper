42c20804c09f30923fff173057a488eacb7d035e
0x1z <shikari8285@gmail.com>
2023-01-28 10:57:58 +0530

    added the page number

--- WebCrawler/src/Main.java
+++ WebCrawler/src/Main.java
@@ -11,29 +11,34 @@ import org.jsoup.select.Elements;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        Document html1 = Jsoup.connect("https://wallhaven.cc/toplist").userAgent("Mozilla").get();
+        Long n = 2L;
+        for (Long i = 1L; i <= n; i++) {
+//            System.out.println("https://wallhaven.cc/toplist?page=" + i);
+            Document html1 = Jsoup.connect("https://wallhaven.cc/toplist?page=" + i).userAgent("Mozilla").get();
 
-        Elements eles = html1.select("a[href]");
-        for (Element link : eles) {
 
-            String filStr = link.attr("href");
-            if (filStr.contains("/w/")) {
-                Document html2 = Jsoup.connect(filStr).userAgent("Mozilla").get();
-                Elements elesinner = html2.getElementsByTag("img");
-                for (Element linkinner : elesinner) {
-                    String finalLink = linkinner.attr("src");
-                    if (!finalLink.contains("logo") && !finalLink.contains("avatar")) {
-                        System.out.println(finalLink);
-                        String dlUrl = finalLink;
-                        Long Baseid = (long) (dlUrl.indexOf("wallhaven-"));
-                        Long Startid = Baseid;
-                        Long EndId = Baseid + 20;
-                        String dlName = dlUrl.substring(Math.toIntExact(Startid), Math.toIntExact(EndId));
-                        URL website = new URL(dlUrl);
-                        ReadableByteChannel rbc = Channels.newChannel(website.openStream());
-                        FileOutputStream fos = new FileOutputStream(dlName);
-                        fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-                        Thread.sleep(1000);
+            Elements eles = html1.select("a[href]");
+            for (Element link : eles) {
+
+                String filStr = link.attr("href");
+                if (filStr.contains("/w/")) {
+                    Document html2 = Jsoup.connect(filStr).userAgent("Mozilla").get();
+                    Elements elesinner = html2.getElementsByTag("img");
+                    for (Element linkinner : elesinner) {
+                        String finalLink = linkinner.attr("src");
+                        if (!finalLink.contains("logo") && !finalLink.contains("avatar")) {
+                            System.out.println(finalLink);
+                            String dlUrl = finalLink;
+                            Long Baseid = (long) (dlUrl.indexOf("wallhaven-"));
+                            Long Startid = Baseid;
+                            Long EndId = Baseid + 20;
+                            String dlName = dlUrl.substring(Math.toIntExact(Startid), Math.toIntExact(EndId));
+                            URL website = new URL(dlUrl);
+                            ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+                            FileOutputStream fos = new FileOutputStream(dlName);
+                            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+                            Thread.sleep(1000);
+                        }
                     }
                 }
             }
